39d9238d33eec2fb3797c58bbdefb0ae0dd7d3af
Marco Mernberger <marco.mernberger@uni-marburg.de>
2023-02-10 10:52:05 +0100

    df_from_biologists

--- src/mutility/frames.py
+++ src/mutility/frames.py
@@ -1,17 +1,26 @@
 import pandas as pd
 import re
-from typing import Union, Optional
+from typing import Union, Optional, List
 from pathlib import Path
 from pandas import DataFrame
 
 
+def trim_dna_strings(sequence: str) -> str:
+    sequence = str(sequence)
+    return sequence.strip().upper()
+
+
 def read_excel_from_biologists(
-    infile: Union[Path, str], sheet_name: Optional[Union[str, int]] = 0
+    infile: Union[Path, str],
+    sheet_name: Optional[Union[str, int]] = 0,
+    trim_columns: Optional[List[str]] = None,
+    merge_columns: List[str] = [],
 ) -> DataFrame:
-    df = pd.read_excel(infile, sheet_name=sheet_name)
-    print(df)
-    # whitespace = re.compile(r"\s+")
-    for col in df.columns:
-        if df[col].dtype == "object":
-            df[col].str.strip()
+    converters = None
+    if trim_columns is not None:
+        converters = dict(zip(trim_columns, [trim_dna_strings] * len(trim_columns)))
+    df = pd.read_excel(infile, sheet_name=sheet_name, converters=converters)
+    # deal with merged cells
+    for col in merge_columns:
+        df[col] = df[col].fillna(method="ffill")
     return df
