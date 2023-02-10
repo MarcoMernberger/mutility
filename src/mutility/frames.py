a270d562b4fb5c9db6980551ebf8bec082e32b32
Marco Mernberger <marco.mernberger@uni-marburg.de>
2023-02-10 11:08:11 +0100

    kwargs in frame_from_biologists

--- src/mutility/frames.py
+++ src/mutility/frames.py
@@ -6,9 +6,9 @@ from pandas import DataFrame
 
 
 def read_excel_from_biologists(
-    infile: Union[Path, str], sheet_name: Optional[Union[str, int]] = 0
+    infile: Union[Path, str], sheet_name: Optional[Union[str, int]] = 0, **kwargs
 ) -> DataFrame:
-    df = pd.read_excel(infile, sheet_name=sheet_name)
+    df = pd.read_excel(infile, sheet_name=sheet_name, **kwargs)
     print(df)
     # whitespace = re.compile(r"\s+")
     for col in df.columns:
