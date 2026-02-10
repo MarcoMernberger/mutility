f2a97c61ed14812516d2bbeed34f2cfce7e51a08
Marco Mernberger <marco.mernberger@uni-marburg.de>
2026-02-10 15:41:40 +0100

    removed pgk_resources dependencvy

--- src/mutility/__init__.py
+++ src/mutility/__init__.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
-from pkg_resources import get_distribution, DistributionNotFound
-
 try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = __name__
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
+    # Py3.8+
+    from importlib.metadata import (
+        version as _get_version,
+        PackageNotFoundError as _PackageNotFoundError,
+    )
+except Exception:
+    # Fallback for older python
+    from importlib_metadata import version as _get_version, PackageNotFoundError as _PackageNotFoundError  # type: ignore
+
+# Change here if project/distribution name differs from package name
+dist_name = __name__
+try:
+    __version__ = _get_version(dist_name)
+except _PackageNotFoundError:
     __version__ = "unknown"
 finally:
-    del get_distribution, DistributionNotFound
+    del _get_version, _PackageNotFoundError
 
 from .functions import *  # noqa: E403
 from .frames import read_excel_from_biologists  # noqa: E401
@@ -19,3 +27,14 @@ from .genomics import (
 )
 from .mutalizer import CodonComparison, extract_codon_from_sequence  # noqa: E401
 from .fastq import count_most_common_sequences
+
+
+__all__ = [
+    "reverse_complement",
+    "get_one_letter_amino_acid_code",
+    "get_three_letter_amino_acid_code",
+    "CodonComparison",
+    "extract_codon_from_sequence",
+    "read_excel_from_biologists",
+    "count_most_common_sequences",
+]
